42b730241b2e4bb0e53b97233113315d62dac254
minicube707 <florentm.contact@gmail.com>
2025-12-09 00:24:39 +0100

    UV TechwithTim

---
+++ Tech with Tim/UV/Demo1/main.py
@@ -0,0 +1,14 @@
+# /// script
+# requires-python = ">=3.13"
+# dependencies = [
+#     "requests>=2.32.5",
+#     "rich>=14.2.0",
+# ]
+# ///
+
+
+import sys
+from rich import print
+import requests
+
+print(sys.version)
---
+++ Tech with Tim/UV/Demo2/main.py
@@ -0,0 +1,6 @@
+
+def main():
+    print("Hello form uv-demo2!!")
+
+if __name__ == __main__:
+        main()
