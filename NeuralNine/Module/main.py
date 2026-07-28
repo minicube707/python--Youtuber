a7388751f5ffb7232a125a4a15b4a735b8f613c7
minicube707 <florentm.contact@gmail.com>
2026-07-28 15:20:35 +0200

    NeuralNine - Module - Add comment

--- NeuralNine/Module/main.py
+++ NeuralNine/Module/main.py
@@ -1,25 +1,29 @@
-
-#Import the functions
+# Import individual functions from the "fonctionality" module
 from fonctionality import add
 from fonctionality import sub
 from fonctionality import mul
 from fonctionality import div
-#Same as:
+
+# Equivalent to importing all functions from the module
 # from fonctionality import *
 
-#Import the file
+# Import everything from the "othermodule" package
 from othermodule import *
-from submodule import fourth
- 
-#Same as: 
-# from othermodule import second 
-# from othermodule import third 
 
+# Import only the "fourth" module from the "submodule" package
+from submodule import fourth
+
+# Equivalent to:
+# from othermodule import second
+# from othermodule import third
+
+# Call the imported mathematical functions
 print(add(2, 3))
 print(sub(2, 3))
 print(mul(2, 3))
 print(div(2, 3))
 
+# Call functions from the imported modules
 second.myfunction()
 third.myotherfunction()
 fourth.last_function()
--- NeuralNine/Module/othermodule/__init__.py
+++ NeuralNine/Module/othermodule/__init__.py
@@ -1,5 +1,16 @@
 
+# The `__init__.py` file is used to turn a directory into a Python package and to control what is exposed when the package is imported.
+# It can be empty, but it is often used to initialize the package, import commonly used modules or functions, and define the `__all__` variable.
+# The `__all__` list specifies which modules or objects are imported when someone uses `from package import *`.
+# This makes the package easier to use and helps organize its public interface by exposing only the intended modules, functions, or classes.
+
+
+# Define which modules are imported when using:
+# from othermodule import *
 __all__ = ["second", "third"]
 
+# Import the function "myfunction" from the "second" module
 from .second import myfunction
+
+# Import the function "myotherfunction" from the "third" module
 from .third import myotherfunction
